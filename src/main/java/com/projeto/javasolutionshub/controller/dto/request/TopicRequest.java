d65353212a315167570371a7dd2e81036cbfb026
Matheus Ferreira Matos <matheus.matos@bandtec.com.br>
2024-01-10 09:56:33 -0300

    Add validations and customize messages

--- src/main/java/com/projeto/javasolutionshub/controller/dto/request/TopicRequest.java
+++ src/main/java/com/projeto/javasolutionshub/controller/dto/request/TopicRequest.java
@@ -1,20 +1,21 @@
 package com.projeto.javasolutionshub.controller.dto.request;
 
-import com.projeto.javasolutionshub.entity.Category;
 import com.projeto.javasolutionshub.entity.Member;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class TopicRequest {
 
-    @NotBlank @NotNull
+    @NotBlank(message = "{validation.titleNotBlank}")
+    @Size(min = 5, max = 100, message = "{validation.titleSize}")
     private String title;
-    @NotBlank @NotNull
+    @NotBlank(message = "{validation.messageNotBlank}")
+    @Size(max = 1000, message = "{validation.messageSize}")
     private String message;
-    @NotNull
+    @NotNull(message = "{validation.categoryIdNotNull}")
     private Long categoryId;
     private Member author;
 
